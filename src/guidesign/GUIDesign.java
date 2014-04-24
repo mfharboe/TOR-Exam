8812fc479b95ad583b842018f37d4f9215832647
Morten <Morten@Morten-Pc>
2014-04-24 13:28:36 +0200

    Design

---
+++ src/guidesign/GUIDesign.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guidesign;
+
+/**
+ *
+ * @author Morten
+ */
+public class GUIDesign {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
