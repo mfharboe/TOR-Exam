a87d52c2af7fd68b4e7138c0bdca8afbe2eab91e
Morten <Morten@Morten-Pc>
2014-04-25 12:48:08 +0200

    Design

--- src/guidesign/fremDesign.java
+++ src/guidesign/fremDesign.java
@@ -37,6 +37,7 @@ public class fremDesign extends javax.swing.JFrame {
         jButton4 = new javax.swing.JButton();
         jButton8 = new javax.swing.JButton();
         jButton7 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
         jPanel4 = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
         jTextField3 = new javax.swing.JTextField();
@@ -53,6 +54,7 @@ public class fremDesign extends javax.swing.JFrame {
         jScrollPane3 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(null);
@@ -69,7 +71,7 @@ public class fremDesign extends javax.swing.JFrame {
         jScrollPane2.setViewportView(jList2);
 
         jPanel3.add(jScrollPane2);
-        jScrollPane2.setBounds(20, 50, 240, 280);
+        jScrollPane2.setBounds(20, 40, 240, 430);
 
         jPanel6.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Min Indsats", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Calibri", 0, 18))); // NOI18N
         jPanel6.setLayout(null);
@@ -105,10 +107,16 @@ public class fremDesign extends javax.swing.JFrame {
         jPanel1.setBounds(10, 160, 190, 120);
 
         jPanel3.add(jPanel6);
-        jPanel6.setBounds(270, 40, 210, 290);
+        jPanel6.setBounds(270, 180, 210, 290);
+
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Foto");
+        jLabel2.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+        jPanel3.add(jLabel2);
+        jLabel2.setBounds(270, 40, 210, 130);
 
         getContentPane().add(jPanel3);
-        jPanel3.setBounds(20, 230, 490, 340);
+        jPanel3.setBounds(20, 230, 490, 480);
 
         jPanel4.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Indsats", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Calibri", 0, 24))); // NOI18N
         jPanel4.setLayout(null);
@@ -147,15 +155,15 @@ public class fremDesign extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Calibri", 0, 72)); // NOI18N
         jLabel1.setText("LOGO");
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(880, 50, 340, 70);
+        jLabel1.setBounds(870, 80, 340, 70);
 
-        jButton5.setText("Gem og Afslut Indsats");
+        jButton5.setText("Gem & Afslut Indsats");
         getContentPane().add(jButton5);
-        jButton5.setBounds(1090, 580, 160, 40);
+        jButton5.setBounds(1090, 720, 160, 40);
 
         jButton2.setText("Hold Leder");
         getContentPane().add(jButton2);
-        jButton2.setBounds(920, 580, 110, 40);
+        jButton2.setBounds(390, 720, 110, 40);
         getContentPane().add(jPanel2);
         jPanel2.setBounds(520, 250, 10, 10);
 
@@ -176,14 +184,18 @@ public class fremDesign extends javax.swing.JFrame {
         jScrollPane3.setViewportView(jTable1);
 
         jPanel5.add(jScrollPane3);
-        jScrollPane3.setBounds(10, 40, 720, 290);
+        jScrollPane3.setBounds(10, 40, 720, 430);
 
         getContentPane().add(jPanel5);
-        jPanel5.setBounds(520, 230, 740, 340);
+        jPanel5.setBounds(520, 230, 740, 480);
 
         jButton1.setText("Fejl & Mangler");
         getContentPane().add(jButton1);
-        jButton1.setBounds(780, 580, 130, 40);
+        jButton1.setBounds(240, 720, 130, 40);
+
+        jButton6.setText("Send Brandragt til Vask");
+        getContentPane().add(jButton6);
+        jButton6.setBounds(40, 720, 180, 40);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -228,6 +240,7 @@ public class fremDesign extends javax.swing.JFrame {
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
     private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
     private javax.swing.JComboBox jComboBox1;
@@ -235,6 +248,7 @@ public class fremDesign extends javax.swing.JFrame {
     private javax.swing.JComboBox jComboBox4;
     private com.toedter.calendar.JDateChooser jDateChooser1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JList jList2;
