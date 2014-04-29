aefa4dfb103e889cce931534ccefeb7c7f1bcf04
Morten <Morten@Morten-Pc>
2014-04-29 10:44:15 +0200

    Brand og Redning

--- src/BE/BERoleTime.java
+++ src/BE/BERoleTime.java
@@ -15,7 +15,7 @@ public class BERoleTime {
     private BERole m_role;
     private BEVehicle m_vehicle;
     private int m_hours;
-    private boolean m_isDone:
+    private boolean m_isDone;
     
     
     public BERoleTime(BEFireman fireman, BEIncident incident, BERole role, BEVehicle vehicle, int hours, boolean isdone){
@@ -26,6 +26,90 @@ public class BERoleTime {
         m_hours = hours;
         m_isDone = isdone;
         
+    }
+
+    /**
+     * @return the m_fireman
+     */
+    public BEFireman getM_fireman() {
+        return m_fireman;
+    }
+
+    /**
+     * @param m_fireman the m_fireman to set
+     */
+    public void setM_fireman(BEFireman m_fireman) {
+        this.m_fireman = m_fireman;
+    }
+
+    /**
+     * @return the m_incident
+     */
+    public BEIncident getM_incident() {
+        return m_incident;
+    }
+
+    /**
+     * @param m_incident the m_incident to set
+     */
+    public void setM_incident(BEIncident m_incident) {
+        this.m_incident = m_incident;
+    }
+
+    /**
+     * @return the m_role
+     */
+    public BERole getM_role() {
+        return m_role;
+    }
+
+    /**
+     * @param m_role the m_role to set
+     */
+    public void setM_role(BERole m_role) {
+        this.m_role = m_role;
+    }
+
+    /**
+     * @return the m_vehicle
+     */
+    public BEVehicle getM_vehicle() {
+        return m_vehicle;
+    }
+
+    /**
+     * @param m_vehicle the m_vehicle to set
+     */
+    public void setM_vehicle(BEVehicle m_vehicle) {
+        this.m_vehicle = m_vehicle;
+    }
+
+    /**
+     * @return the m_hours
+     */
+    public int getM_hours() {
+        return m_hours;
+    }
+
+    /**
+     * @param m_hours the m_hours to set
+     */
+    public void setM_hours(int m_hours) {
+        this.m_hours = m_hours;
+    }
+
+    /**
+     * @return the m_isDone
+     */
+    public boolean isM_isDone() {
+        return m_isDone;
+    }
+
+    /**
+     * @param m_isDone the m_isDone to set
+     */
+    public void setM_isDone(boolean m_isDone) {
+        this.m_isDone = m_isDone;
     }
     
 }
